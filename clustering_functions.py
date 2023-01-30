132c02d115f6ab1c56480d83dbf92e957e417880
stephen-c-perry <stephen.christian.perry@gmail.com>
2023-01-30 14:17:47 -0600

    finished exercises

--- clustering_functions.py
+++ clustering_functions.py
@@ -9,29 +9,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
 
-
-
-
 def plot_cluster_inertia_k(train, col1, col2):
 
 #initialize variable to be reference later, taking in arguments for features/columns
-    X = train[[col1, col2]]
 
-#fit kmeans algo to columns then predict
+    X = train[[col1, col2]]
 
 #scaling
     scaler = MinMaxScaler()
-    X = scaler.fit_transform(X)
+    X= scaler.fit_transform(X)
 
+#fit KMeans algo
     kmeans = KMeans(n_clusters = 3)
-
     kmeans.fit(X)
 
 #assign predictions to original dataframe
     train['clustering'] = kmeans.predict(X)
 
 #plot cluster predictions, take in argument for column of predictions
-    X_plot = sns.scatterplot(x = train[col1], y = train[col2], hue = train['clustering'])
+    sns.scatterplot(x = train[col1], y = train[col2], hue = train['clustering'])
 
 #plot inertia over k increasing, take in argument for column of predictions
     with plt.style.context('seaborn-whitegrid'):
@@ -41,12 +37,4 @@ def plot_cluster_inertia_k(train, col1, col2):
         plt.xlabel('k')
         plt.ylabel('inertia')
         plt.title('Change in inertia as k increases')
-
-
-
-'''
-Questions:
-when using the above function,
-Should I train, val_test split before plotting?  
-Should I scale before plotting?
-'''
+    
